1ddd4b3bc392c2a6721824fd6778f9256f9760e2
Bhone Htet Naing <naingbhonehtet70@gmail.com>
2026-02-20 11:53:55 +0630

    add @tailwind/postcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue,svelte}", // Add paths for all your template files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
